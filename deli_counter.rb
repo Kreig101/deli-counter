2b8a7ae6ade22f8b5a8654fc057ba90e1b395703
Craig Storer <craigstorer@gmail.com>
2019-12-09 21:04:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,16 @@
-def line(names)
 
+def line(names)
+  display = []
 if names.count == 0
   puts "The line is currently empty."
+    elsif names.count >= 1 
+    names.each_with_index(1) do |x,y|
+    display <<  puts "The line is currently: #{y}. #{x}"
 
    
  
 end
+return display
 end
+end
+
